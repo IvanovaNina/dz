80c1ffc3ebd0aa388b8f8b63acdacc11b85e8d6b
Nina <naneska-1@yandex.ru>
2022-09-17 12:00:21 +0600

    first commit

---
+++ zad2/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, которая на вход принимает 2 числа и выдает какое число больше, а какое меньше
+
+Console.WriteLine("Введите число: ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num2 > num1)
+{
+    Console.WriteLine ($"Наибольшее число - {num2}, наименьшее число- {num1}");
+}
+else
+{
+    Console.WriteLine ($"Наибольшее число - {num1}, наименьшее число- {num2}");
+}
---
+++ zad4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая на вход принимает 3 числа и выдаёт максимальное из этих чисел
+Console.WriteLine("Введите число: ");
+
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num3 > num1 && num3 > num2)
+{
+    Console.WriteLine ($"Наибольшее число - {num3}");
+}
+else if (num2 > num1 && num2 > num3)
+{
+    Console.WriteLine ($"Наибольшее число - {num2}");
+}
+else 
+{
+    Console.WriteLine ($"Наибольшее число - {num1}");
+}
